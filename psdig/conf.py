881c8cd17da3b7f8d14df6bd65a144ae7d21a4a8
root <feiphilchen@gmail.com>
2023-11-30 05:27:25 +0000

    fd cleanup in FdResolve

--- psdig/conf.py
+++ psdig/conf.py
@@ -1,4 +1,4 @@
 LOGGER_NAME="psdig"
 BPF_OBJ_DIR="/usr/local/share/psdig/bpf"
 TRACEFS="/sys/kernel/debug/tracing/events"
-
+FD_CACHE=1024
--- psdig/file_processor.py
+++ psdig/file_processor.py
@@ -1,23 +1,35 @@
 import os
 import logging
-from .conf import LOGGER_NAME
+import time
+from .conf import LOGGER_NAME,FD_CACHE
 
-class FileResolve(object):
+class FdResolve(object):
     def __init__(self):
         self.logger = logging.getLogger(LOGGER_NAME)
         self.fd_hash = {}
 
+    def fd_key(self, pid, fd):
+        return f"{pid}:{fd}"
+
+    def cleanup(self, limited):
+        sorted_hash = sorted(self.fd_hash, key=lambda x: x[1]['update'])
+        for pos in range(0, limited):
+            key = sorted_hash[0]
+            del self.fd_hash[key]
+
     def file_add(self, pid, fd, filename):
-        if pid not in self.fd_hash:
-            self.fd_hash[pid] = {}
-        self.fd_hash[pid][fd] = filename
+        key = self.fd_key(pid, fd)
+        time_now = time.time()
+        self.fd_hash[key] = {"filename":filename, "update":time_now}
+        if len(self.fd_hash) > FD_CACHE:
+            self.cleanup(int(FD_CACHE/2))
 
     def file_lookup(self, pid, fd):
-        if pid not in self.fd_hash:
+        key = self.fd_key(pid, fd)
+        if key not in self.fd_hash:
             return None
-        if fd not in self.fd_hash[pid]:
-            return None
-        return self.fd_hash[pid][fd]
+        self.fd_hash[key]['update'] = time.time()
+        return self.fd_hash[key]['filename']
 
     def syscall(self, metadata, name, args, ret):
         pid = metadata['pid']
